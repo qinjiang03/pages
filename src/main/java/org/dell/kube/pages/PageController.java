a9feae339127984d984ae9d870cdd81df8638c2a
qinjiang <qinjiang03@gmail.com>
2020-10-30 02:51:30 +0000

    Added logging

--- src/main/java/org/dell/kube/pages/PageController.java
+++ src/main/java/org/dell/kube/pages/PageController.java
@@ -1,11 +1,16 @@
 package org.dell.kube.pages;
+
+import ch.qos.logback.classic.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 @RestController
 @RequestMapping("/pages")
 public class PageController {
+    Logger logger =(Logger) LoggerFactory.getLogger(this.getClass());
     private IPageRepository pageRepository;
     public PageController(IPageRepository pageRepository)
     {
@@ -14,19 +19,29 @@ public class PageController {
     @PostMapping
     public ResponseEntity<Page> create(@RequestBody Page page) {
         Page newPage= pageRepository.create(page);
+        logger.info("CREATE-INFO:Creating page with id = ");
+        logger.debug("CREATE-DEBUG:Creating page with id = ");
         return new ResponseEntity<Page>(newPage, HttpStatus.CREATED);
     }
     @GetMapping("{id}")
     public ResponseEntity<Page> read(@PathVariable long id) {
+
+        logger.info("READ-INFO:Fetching page with id = " + id);
+        logger.debug("READ-DEBUG:Fetching page with id = " + id);
+
         Page page = pageRepository.read(id);
         if(page!=null)
             return new ResponseEntity<Page>(page, HttpStatus.OK);
-        else
+        else {
+            logger.error("READ-ERROR:Could not find page with id = " + id);
             return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
     }
     @GetMapping
     public ResponseEntity<List<Page>> list() {
         List<Page> pages= pageRepository.list();
+        logger.info("READ-INFO:Fetchhing all pages with id = " );
+        logger.debug("READ-DEBUG:Fetching all page with id = " );
         return new ResponseEntity<List<Page>>(pages, HttpStatus.OK);
     }
     @PutMapping("{id}")
